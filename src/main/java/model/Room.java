709034a9daf72d93cc0cf4fbeddf74c7ffacad8a
Anna-Kohanova <anitacx88@gmail.com>
2016-09-20 23:49:59 +0300

    Ready connection with client using Retrofit2. Simple json sending

---
+++ src/main/java/model/Room.java
@@ -0,0 +1,15 @@
+package model;
+
+/**
+ * Room object
+ * Created by Ania on 20.09.2016.
+ */
+public class Room {
+    private String name;
+    private int number;
+
+    public Room(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+}
--- src/main/java/servlets/GetRoomsServlet.java
+++ src/main/java/servlets/GetRoomsServlet.java
@@ -1,17 +1,19 @@
 package servlets;
 
+import com.google.gson.Gson;
+import model.Room;
+
 import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
+ * Servlet for interacting with client side
  * Created by Ania on 17.09.2016.
  */
 
@@ -20,11 +22,18 @@ public class GetRoomsServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("==========================");
 
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("asdf") ;
-        out.flush();
-//        response.getWriter().print("It works!!!");
+        Room r1 = new Room("asd", 1);
+        Room r2 = new Room("qwert", 2);
+
+        List<Room> rooms = new ArrayList<Room>();
+        rooms.add(r1);
+        rooms.add(r2);
+
+        String json = new Gson().toJson(rooms);
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
